319357f01ec3e54fd06a0104057d9f26e29f97a5
Cormac D <BuilderOfTheFuture@gmail.com>
2024-07-28 19:46:15 -0700

    Typo fix one file builder

--- create_one_files.py
+++ create_one_files.py
@@ -10,6 +10,6 @@ root = os.path.dirname(os.path.realpath('create_one_files.py'))
 
 csv_paths = ['/battery_alt_dataset/regular_alt_batteries',
              '/battery_alt_dataset/second_life_batteries',
-             '/battery_alt_dataset/recommissoned_batteries']
+             '/battery_alt_dataset/recommissioned_batteries']
 for csv_path in csv_paths:
     csv_combine.concatinateCSVs(root+ csv_path, ignore_list=[]).to_csv(root + csv_path + '.csv', index=False)
